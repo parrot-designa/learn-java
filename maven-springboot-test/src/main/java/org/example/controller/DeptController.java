187a3c078a8e253e78d6c1c7616ed3875fe8af44
wujiabao <wujiabao@leateckgroup.com>
2025-12-18 16:12:09 +0800

    Controller Service Dao三层架构

--- maven-springboot-test/src/main/java/org/example/controller/DeptController.java
+++ maven-springboot-test/src/main/java/org/example/controller/DeptController.java
@@ -1,39 +1,16 @@
 package org.example.controller;
 
-import org.apache.commons.io.IOUtils;
-import org.example.entity.Dept;
 import org.example.entity.Result;
+import org.example.service.DeptServiceImpl;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.InputStream;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class DeptController {
 
-    public List<Dept> getDeptList(){
-        // 获取部门流
-        InputStream input = this.getClass().getClassLoader().getResourceAsStream("dept.txt");
-        List<String> strings = IOUtils.readLines(input,"utf-8");
-
-        List<Dept> depts = strings.stream().map((str) -> {
-            String[] paths = str.split("，");
-            Integer id = Integer.valueOf(paths[0]);
-            String name = paths[1];
-            String updateTime = paths[2];
-            return new Dept(id,name,updateTime);
-        }).collect(Collectors.toList());
-
-        return depts;
-    }
-
 //    @RequestMapping(value = "/dept/list",method = RequestMethod.GET)
     @GetMapping("/dept")
     public Result getAll(){
-        return Result.success(getDeptList());
+        return Result.success(DeptServiceImpl.getList());
     }
 }
---
+++ maven-springboot-test/src/main/java/org/example/service/DeptServiceImpl.java
@@ -0,0 +1,22 @@
+package org.example.service;
+
+import org.example.dao.DeptDao;
+import org.example.entity.Dept;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DeptServiceImpl {
+    public static List<Dept> getList(){
+        DeptDao deptDao = new DeptDao();
+        List<String> strings = deptDao.getDeptString();
+        List<Dept> depts = strings.stream().map((str) -> {
+            String[] paths = str.split("，");
+            Integer id = Integer.valueOf(paths[0]);
+            String name = paths[1];
+            String updateTime = paths[2];
+            return new Dept(id,name,updateTime);
+        }).collect(Collectors.toList());
+        return depts;
+    }
+}
